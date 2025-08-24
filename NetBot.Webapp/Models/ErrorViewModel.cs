4006a1a55f42b0c27ac1b2a8dd3d747cff837e44
RaffDevs <rveronezdeveloper@gmail.com>
2025-08-24 20:40:41 -0300

    first commit

---
+++ NetBot.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NetBot.Webapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
